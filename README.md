93815434dfdecbf757fc0ee34a0a474a5d41bcf0
Hqnnqh <hannah@diefluchs.at>
2024-12-11 14:07:57 +0100

    feat: implement patching script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # patchpt
+
+Patch Cisco PacketTracer 8.2.2 to use the ActivityWizard without entering a password.
+> Tested on x86_64 Linux
--- patch.sh
+++ patch.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-echo Patching PacketTracer 8.2.2
+JZ_OFFSET=0x368dacc
+PT_FILE=$1
+
+EXPECTED="0f84"
+PATCH="\x48\xe9"
+
+set -e
+
+if [ -z "$PT_FILE" ]; then
+    echo "Usage: $0 <PacketTracer binary file>"
+    exit 1
+fi
+
+echo Checking for patch compatibility...
+
+cd "$PT_PATH"
+
+FOUND=$(xxd -s $JZ_OFFSET -l 2 "$PT_FILE" | awk '{print $2}')
+
+if [ "$EXPECTED" = "$FOUND" ]; then
+    echo Patching PacketTracer 8.2.2...
+    echo -ne "$PATCH" | dd conv=notrunc bs=1 seek=$(("$JZ_OFFSET")) of="$PT_FILE"
+
+    exit
+fi
+echo This PacketTracer binary is not compatible with the patch.
