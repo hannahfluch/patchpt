50758470e0fa236d152ab78aed88b86702032cce
Hqnnqh <hannah@diefluchs.at>
2025-04-27 20:45:57 +0200

    feat: check for existing patch

--- patch.sh
+++ patch.sh
@@ -4,6 +4,7 @@ JZ_OFFSET=0x368dacc
 PT_FILE=$1
 
 EXPECTED="0f84"
+PATCHED="48e9"
 PATCH="\x48\xe9"
 
 set -e
@@ -22,7 +23,12 @@ FOUND=$(xxd -s $JZ_OFFSET -l 2 "$PT_FILE" | awk '{print $2}')
 if [ "$EXPECTED" = "$FOUND" ]; then
     echo Patching PacketTracer 8.2.2...
     echo -ne "$PATCH" | dd conv=notrunc bs=1 seek=$(("$JZ_OFFSET")) of="$PT_FILE"
-
-    exit
+	echo Patch applied successfully.
+    exit 0
+elif [ "$PATCHED" = "$FOUND" ]; then
+    echo PacketTracer has already been patched.
+    exit 0
+else
+    echo This PacketTracer binary is not compatible with the patch.
+    exit 1
 fi
-echo This PacketTracer binary is not compatible with the patch.
